8b0032da14f1898566c03ce41124b97fa61cbdb1
Developer <denbudeyko@yandex.ru>
2019-05-31 06:49:29 +0300

    reviews

---
+++ markup/components/reviews/reviews.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+
+  var reviewsSlides = $('.reviews-slides')
+  reviewsSlides.owlCarousel({
+    items: 1,
+    dots: false,
+    loop: true,
+    nav: true
+  })
+})
