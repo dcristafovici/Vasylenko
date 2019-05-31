30e5efc6c8791b5d076e44349af126ed670ed0b4
Developer <denbudeyko@yandex.ru>
2019-05-31 07:59:37 +0300

    service

---
+++ markup/components/header/header.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+  
+  $("nav.menu").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1500);
+  });
+  
+
+
+  $('.home-action').on('click',function(){
+    
+    var top = $('#about').offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1500);
+  })
+
+})
